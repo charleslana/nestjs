9dcc2e328d26b8067491dbe1a28124168713082e
Charles <charleslanop@gmail.com>
2021-06-23 20:46:58 -0300

    feat(structure) update

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Param, Post} from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+    @Get()
+    findAll() {
+        return 'List of courses';
+    }
+
+    @Get(':id')
+    findById(@Param('id') id: string) {
+        return `Course #${id}`;
+    }
+
+    @Post()
+    create(@Body() body: any) {
+        return body;
+    }
+}
